c85de79465133c8a2caba7115346aed427d720c7
Zydrax04 <49263892+Zydrax04@users.noreply.github.com>
2020-11-11 23:44:18 +0200

    varianta java

--- gamev10/app/src/main/java/com/example/myapplication/WelcomePage.java
+++ gamev10/app/src/main/java/com/example/myapplication/WelcomePage.java
@@ -24,17 +24,43 @@ public class WelcomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
+
         Intent intent = getIntent();
         String username = intent.getStringExtra("USER_NAME");
         Toast.makeText(WelcomePage.this, "Welcome " + username, Toast.LENGTH_SHORT).show();
 
         first_card = findViewById(R.id.first_card);
 
+        // Points to the root reference
+        //var storageRef = firebase.storage().ref();
+
+        // Points to 'images'
+        StorageReference imagesRef = storageReference.child("CardImages");
+
+        // Points to 'images/space.jpg'
+        // Note that you can use variables to create child values
+        String fileName = "BasicCardSprite.jpeg";
+        StorageReference spaceRef = imagesRef.child(fileName);
+
+        // File path is 'images/space.jpg'
+        String path = spaceRef.getPath();
+
+        // File name is 'space.jpg'
+        String name = spaceRef.getName();
+
+        // Points to 'images'
+        //StorageReference imagesRef2 = spaceRef.getParent();
+
 // Download directly from StorageReference using Glide
 // (See MyAppGlideModule for Loader registration)
+        try{
         Glide.with(this /* context */)
-                .load(storageReference)
-                .into(imageView);
+                .load(spaceRef.getDownloadUrl())
+                .into(first_card);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }

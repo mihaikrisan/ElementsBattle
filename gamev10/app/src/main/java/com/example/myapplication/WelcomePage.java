636aca5cf4bb09bbdac7729a63a9ddec25dd5d56
mihai.krisan <mihai.krisan@yahoo.com>
2020-11-11 22:14:20 +0200

    some stuff

--- gamev10/app/src/main/java/com/example/myapplication/WelcomePage.java
+++ gamev10/app/src/main/java/com/example/myapplication/WelcomePage.java
@@ -1,12 +1,24 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 public class WelcomePage extends AppCompatActivity {
+    ImageView first_card;
+    StorageReference storageReference = FirebaseStorage.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +27,14 @@ public class WelcomePage extends AppCompatActivity {
         Intent intent = getIntent();
         String username = intent.getStringExtra("USER_NAME");
         Toast.makeText(WelcomePage.this, "Welcome " + username, Toast.LENGTH_SHORT).show();
+
+        first_card = findViewById(R.id.first_card);
+
+// Download directly from StorageReference using Glide
+// (See MyAppGlideModule for Loader registration)
+        Glide.with(this /* context */)
+                .load(storageReference)
+                .into(imageView);
     }
 
 }
